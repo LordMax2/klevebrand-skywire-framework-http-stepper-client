c85625ba4dde68d58701306c76cea7929b56e3d0
LordMax2 <max.klevebrand@gmail.com>
2025-12-18 18:03:46 +0100

    Init

---
+++ include/klevebrand-skywire-http-stepper-client.h
@@ -0,0 +1,47 @@
+#ifndef KLEVEBRAND_SKYWIRE_HTTP_STEPPER_CLIENT_H
+#define KLEVEBRAND_SKYWIRE_HTTP_STEPPER_CLIENT_H
+
+#include "Arduino.h"
+#include "klevebrand-skywire-framework.h"
+
+class SkywireHttpStepperClient
+{
+public:
+	SkywireHttpStepperClient(HardwareSerial skywire_serial, String base_url, int port) : skywire(skywire_serial)
+	{
+		this->base_url = base_url;
+		this->port = port;
+
+		resetState();
+	}
+	bool start();
+	SkywireResponseResult_t get(String path);
+
+private:
+	Skywire skywire;
+	String base_url;
+	int port;
+
+	bool httpCfg();
+	bool http_cfg_sent = false;
+	bool http_cfg_ok_recieved = false;
+
+	bool httpQry(String path);
+	bool http_qry_sent = false;
+	bool http_qry_ok_recieved = false;
+
+	bool httpRcv();
+	bool http_rcv_sent = false;
+	bool http_rcv_ok_recieved = false;
+
+	bool httpRing();
+	bool http_ring_recieved = false;
+
+	void resetState();
+	void serialReadToRxBuffer();
+
+	void resetRxBuffer();
+	String rx_buffer;
+};
+
+#endif // KLEVEBRAND_SKYWIRE_HTTP_STEPPER_CLIENT_H
---
+++ src/klevebrand-skywire-http-stepper-client.cpp
@@ -0,0 +1,167 @@
+#include "klevebrand-skywire-http-stepper-client.h"
+
+void SkywireHttpStepperClient::resetState()
+{
+	http_cfg_ok_recieved = false;
+	http_cfg_sent = false;
+	http_qry_ok_recieved = false;
+	http_qry_sent = false;
+	http_ring_recieved = false;
+	http_rcv_sent = false;
+
+	resetRxBuffer();
+}
+
+void SkywireHttpStepperClient::resetRxBuffer()
+{
+	rx_buffer = "";
+}
+
+bool SkywireHttpStepperClient::start()
+{
+	skywire.start();
+	return true;
+}
+
+SkywireResponseResult_t SkywireHttpStepperClient::get(String path)
+{
+	if(!httpCfg()) {
+		return SkywireResponseResult_t(false, "");
+	} 
+
+	if(!httpQry(path)) {
+		return SkywireResponseResult_t(false, "");
+	}
+
+	if(!httpRing()) {
+		return SkywireResponseResult_t(false ,"");
+
+	}
+
+	if(!httpRcv()) {
+		return SkywireResponseResult_t(false, "");
+	}
+
+
+	serialReadToRxBuffer();
+
+	if(DEBUG) {
+		Serial.println(rx_buffer);
+	}
+
+	resetState();
+
+	return SkywireResponseResult_t(true, rx_buffer);
+}
+
+bool SkywireHttpStepperClient::httpCfg()
+{
+	if (!http_cfg_sent)
+	{
+		skywire.print("AT#HTTPCFG=0,\"" + base_url + "\"\r");
+
+		http_cfg_sent = true;
+
+		return false;
+	}
+
+	serialReadToRxBuffer();
+
+	if (http_cfg_sent && !http_cfg_ok_recieved)
+	{
+		http_cfg_ok_recieved = rx_buffer.indexOf("OK") >= 0;
+
+		if (http_cfg_ok_recieved)
+		{
+			if(DEBUG) {
+				Serial.println("STEPPER CLIENT RECEIVED HTTPCFG OK");
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	return true;
+}
+
+bool SkywireHttpStepperClient::httpQry(String path)
+{
+	if (!http_qry_sent)
+	{
+		if(DEBUG) {
+			Serial.println("Sending QRY");
+		}
+
+		http_qry_sent = skywire.print("AT#HTTPQRY=0,0,\"/" + path + "\"\r");
+
+		return false;
+	}
+
+	serialReadToRxBuffer();
+
+	if (!http_qry_ok_recieved)
+	{
+		http_qry_ok_recieved = rx_buffer.indexOf("OK\r") >= 0;
+
+		if (http_qry_ok_recieved)
+		{
+			if(DEBUG) {
+				Serial.println("STEPPER CLIENT RECEIVED HTTPQRY OK");
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	return true;
+}
+
+bool SkywireHttpStepperClient::httpRing()
+{
+	serialReadToRxBuffer();
+
+	if (!http_ring_recieved)
+	{
+		
+		http_ring_recieved = rx_buffer.indexOf("HTTPRING") > 0;
+
+		if (http_ring_recieved)
+		{
+			if(DEBUG) {
+				Serial.println(rx_buffer);
+				Serial.println("STEPPER CLIENT RECEIVED HTTPRING OK");
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	return true;
+}
+
+bool SkywireHttpStepperClient::httpRcv()
+{
+	if (!http_rcv_sent)
+	{
+		http_rcv_sent = skywire.print("AT#HTTPRCV=0\r");
+
+		return false;
+	}
+
+	return true;
+}
+
+void SkywireHttpStepperClient::serialReadToRxBuffer()
+{
+	while (Serial3.available())
+	{
+		char c = Serial3.read();
+		rx_buffer += c;
+	}
+}
